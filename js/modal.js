a6402410558353806119b01e0b47aa8045e392f2
Andrey Pavelko <andreypavelko27@gmail.com>
2026-02-14 00:15:58 +0200

    all done

--- js/modal.js
+++ js/modal.js
@@ -15,4 +15,27 @@
     // is-open це клас який буде додаватися/забиратися на бекдроп при натисканні на кнопки
     refs.modal.classList.toggle('is-open');
   }
+
+  
+})();
+
+(() => {
+  const refs = {
+    // Додати атрибут data-modal-open на кнопку відкриття
+    openModalBtn: document.querySelector('[mobile-modal-open]'),
+    // Додати атрибут data-modal-close на кнопку закриття
+    closeModalBtn: document.querySelector('[mobile-modal-close]'),
+    // Додати атрибут data-modal на бекдроп модалки
+    modal: document.querySelector('[mobile-modal]'),
+  };
+
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    // is-open це клас який буде додаватися/забиратися на бекдроп при натисканні на кнопки
+    refs.modal.classList.toggle('is-open');
+  }
+
+  
 })();
