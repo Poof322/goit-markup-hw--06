e2eea3723a38be99ad2f243b6c2fe6b9190d607e
Andrey Pavelko <andreypavelko27@gmail.com>
2026-02-14 23:20:50 +0200

    23

--- js/modal.js
+++ js/modal.js
@@ -22,11 +22,11 @@
 (() => {
   const refs = {
     // Додати атрибут data-modal-open на кнопку відкриття
-    openModalBtn: document.querySelector('[data-mobile-modal-open]'),
+    openModalBtn: document.querySelector('[data-mobile-open]'),
     // Додати атрибут data-modal-close на кнопку закриття
-    closeModalBtn: document.querySelector('[data-mobile-modal-close]'),
+    closeModalBtn: document.querySelector('[data-mobile-close]'),
     // Додати атрибут data-modal на бекдроп модалки
-    modal: document.querySelector('[data-mobile-modal]'),
+    modal: document.querySelector('[data-mobile]'),
   };
 
   refs.openModalBtn.addEventListener('click', toggleModal);
